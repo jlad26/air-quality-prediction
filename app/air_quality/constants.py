0dbe0264ad9d0ebb70a4fb4a212bc70ace24e39a
Jon Anwyl <jon.anwyl@gmail.com>
2022-10-19 08:07:42 +0200

    Locate .env in cwd or parent

--- app/air_quality/constants.py
+++ app/air_quality/constants.py
@@ -5,10 +5,23 @@ import os
 from dotenv import dotenv_values
 
 # .env file location is dependent on evironment. If in docker, it is stored in
-# /app/app_data. Otherwise it is stored in parent of current working directory.
+# /app/app_data. Otherwise we look for it in current working directory then its parent.
+# This is because for prediction web app and updater share a .env in their common parent.
 
+# First look for bind mount.
 HOST_ENV_DIR = '/app/app_data'
-env_dir = HOST_ENV_DIR if os.path.exists(HOST_ENV_DIR) else os.path.dirname(os.getcwd())
+if os.path.exists(HOST_ENV_DIR):
+    env_dir = HOST_ENV_DIR
+
+else:
+
+    # Then check in current working directory.
+    if os.path.exists(os.path.join(os.getcwd(), '.env')):
+        env_dir = os.getcwd()
+
+    # Followed by parent of current working directory.
+    else:
+        env_dir = os.path.dirname(os.getcwd())
 
 env_path = os.path.join(env_dir, '.env')
 if not os.path.exists(env_path):
@@ -21,6 +34,6 @@ for var, value in ENV_VARS.items():
         ENV_VARS[var] = value.lower() == 'true'
 
 # Set the working directory. Default is same folder as .env file.
-WORK_DIR = ENV_VARS['WORK_DIR'] if 'WORK_DIR' in ENV_VARS else os.path.join(os.getcwd(), 'app_data')
+WORK_DIR = ENV_VARS['WORK_DIR'] if 'WORK_DIR' in ENV_VARS else env_dir
 
 LOG_DIR_PATH = os.path.join(WORK_DIR, 'logs')
